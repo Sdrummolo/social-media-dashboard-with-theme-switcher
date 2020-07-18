e5bb7176d06afa66735f1ff85cedb139c752ee0d
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-07-18 16:28:15 +0200

    fixed css bug

--- src/utilities/GlobalStyle.js
+++ src/utilities/GlobalStyle.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap'); 
+  /* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');  */
   * {
     padding: 0;
     margin: 0;
