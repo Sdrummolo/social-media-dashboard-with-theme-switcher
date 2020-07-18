7987e9052da634b8352ebf0ce319292b2d7f8df3
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-07-18 16:24:32 +0200

    Updates

---
+++ precache-manifest.4d4de945a0eb2f1f1d0c2abad41e1065.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd90759b88196ffd780b1005338ff16d",
+    "url": "/social-media-dashboard-with-theme-switcher/index.html"
+  },
+  {
+    "revision": "b0a4e230b7c3766d0a3d",
+    "url": "/social-media-dashboard-with-theme-switcher/static/js/2.7d56ae27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-media-dashboard-with-theme-switcher/static/js/2.7d56ae27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceeee5620ab88b8203b1",
+    "url": "/social-media-dashboard-with-theme-switcher/static/js/main.aad812a6.chunk.js"
+  },
+  {
+    "revision": "49be841f1fc62ee032e6",
+    "url": "/social-media-dashboard-with-theme-switcher/static/js/runtime-main.9bb5ec56.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/social-media-dashboard-with-theme-switcher/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/social-media-dashboard-with-theme-switcher/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/social-media-dashboard-with-theme-switcher/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/social-media-dashboard-with-theme-switcher/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/social-media-dashboard-with-theme-switcher/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/social-media-dashboard-with-theme-switcher/static/media/icon-youtube.8499da12.svg"
+  }
+]);
