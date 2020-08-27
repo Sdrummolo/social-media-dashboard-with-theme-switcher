f1551abea9fc46ae1dd4e1276ed32bafe65d45e9
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-08-27 13:02:09 +0200

    Moved google fonts import from globalStyle to index.html

--- src/utilities/GlobalStyle.js
+++ src/utilities/GlobalStyle.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
-  /* @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');  */
   * {
     padding: 0;
     margin: 0;
